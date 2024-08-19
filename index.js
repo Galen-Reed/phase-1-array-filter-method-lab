f6cb3cb467c67a5fd2f3987d0bf406a6c52c98ea
GalenReed <galenreed@vt.edu>
2024-08-19 19:44:02 -0400

    this stuff is kinda hard

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter(function (element) {
+        return element.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (element) {
+        return element.startsWith(string);
+    });
+}
+function matchName(array, name) {
+    return array.filter(function (element) {
+        return element.name === name;
+    });
+}
